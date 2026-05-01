96c93b2ed81af19c93adc14e3761e37dfc269adf
kdxiaoyi [MiPad7] <kdxiaoyi@outlook.com>
2026-05-01 12:59:21 +0800

    添加url

--- fileupload/src/main/java/org/nanohttpd/fileupload/NanoFileUpload.java
+++ fileupload/src/main/java/org/nanohttpd/fileupload/NanoFileUpload.java
@@ -52,7 +52,9 @@ import org.nanohttpd.protocols.http.request.Method;
 /**
  * @author victor & ritchieGitHub
  * @deprecated NanoHTTPd 的文件上传使用的库存在<b>远程执行漏洞</b>，请勿使用！！
- * @see {@link https://github.com/kdxhub/NanoHTTPd-with-ProxyProtocol/pull/1#issuecomment-4357805479}  该漏洞的详情信息
+ * @see {@link https
+ *      ://github.com/kdxhub/NanoHTTPd-with-ProxyProtocol/pull/1#issuecomment
+ *      -4357805479} 该漏洞的详情信息
  */
 @Deprecated
 public class NanoFileUpload extends FileUpload {
